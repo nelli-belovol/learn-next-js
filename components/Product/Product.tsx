88e2a523c4eaad0c390afa5e301ed976f80b8b58
Nelli-Belovol <nelli199020@gmail.com>
2023-01-27 15:53:53 +0200

    add Image next component and helper

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -6,14 +6,20 @@ import { Card } from "../Card/Card";
 import { Rating } from "../Rating/Rating";
 import { Tag } from "../Tag/Tag";
 import { Button } from "../Button/Button";
-import { priceRu } from "../../helpers/helpers";
+import { declOfNum, priceRu } from "../../helpers/helpers";
 import { Divider } from "../Divider/Divider";
+import Image from "next/image";
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
   return (
     <Card className={styles.product}>
       <div className={styles.logo}>
-        <img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} />
+        <Image
+          width={70}
+          height={70}
+          src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+          alt={product.title}
+        />
       </div>
       <div className={styles.title}>{product.title}</div>
       <div className={styles.price}>
@@ -39,10 +45,20 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
       </div>
       <div className={styles.priceTitle}>цена</div>
       <div className={styles.creditTitle}>кредит</div>
-      <div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+      <div className={styles.rateTitle}>
+        {product.reviewCount} {declOfNum(product.reviewCount, ["отзыв", "отзыва", "отзывов"])}
+      </div>
       <Divider className={styles.hr} />
       <div className={styles.description}>{product.description}</div>
-      <div className={styles.features}>features</div>
+      <div className={styles.features}>
+        {product.characteristics.map((ch) => (
+          <div className={styles.characteristics} key={ch.name}>
+            <span className={styles.characteristicsName}>{ch.name}</span>
+            <span className={styles.characteristicsDots}></span>
+            <span className={styles.characteristicsValue}>{ch.value}</span>
+          </div>
+        ))}
+      </div>
       <div className={styles.advBlock}>
         {product.advantages && (
           <div className={styles.advantages}>
